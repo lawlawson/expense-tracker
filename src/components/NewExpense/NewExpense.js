26672047978030a919176d9d8bdcc24fe9b4f75c
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2021-05-08 00:37:05 +0100

    Fixed issue with styling not showing

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import './NewExpense.css';
+import ExpenseForm from './ExpenseForm';
 
 const NewExpense = () => {
   return (
-    <div>
-      <form className='expense-form' action=''></form>
-    </div>
+    <form className='new-expense'>
+      <ExpenseForm />
+    </form>
   );
 };
 
